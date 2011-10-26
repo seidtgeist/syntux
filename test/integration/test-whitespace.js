340ba100591d7700b890652cec8b76b44d106993
Stephan Seidt <evilhackerdude@gmail.com>
2011-10-26 12:51:06 +0200

    Implement trailing whitespace removal for comments

--- test/integration/test-whitespace.js
+++ test/integration/test-whitespace.js
@@ -6,3 +6,23 @@ var assert = require('assert');
   var source = Syntux.transform('    foo    \n', {whitespace: true});
   assert.equal(source, '    foo\n');
 })();
+
+(function testRemoveTrailingWhitespaceCommentSingle() {
+  var source = Syntux.transform('    // foo    \n', {whitespace: true});
+  assert.equal(source, '    // foo\n');
+})();
+
+(function testRemoveTrailingWhitespaceCommentMulti() {
+  var dirty = '  /*       \n' +
+              '    foo    \n' +
+              '    bar    \n' +
+              '  */       \n' +
+              '    \n';
+  var clean = '  /*\n' +
+              '    foo\n' +
+              '    bar\n' +
+              '  */\n' +
+              '\n';
+  var source = Syntux.transform(dirty, {whitespace: true});
+  assert.equal(source, clean);
+})();
