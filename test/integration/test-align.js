a9bb84db655a8d921c8f143e629bb4c6fa9a08d4
Felix Geisendörfer <felix@debuggable.com>
2011-10-27 21:37:19 +0200

    First somewhat working align plugin

---
+++ test/integration/test-align.js
@@ -0,0 +1,49 @@
+var TransformHelper = require('../common').helper('transform_helper');
+var helper = TransformHelper.create();
+
+helper.test({
+  description:
+    'Simple alignment of two var statements',
+  options:
+    {align: ['=']},
+  input:
+    'var a = 1;\n' +
+    'var muchlonger = 2;\n',
+  expected:
+    'var a          = 1;\n' +
+    'var muchlonger = 2;\n',
+});
+
+helper.test({
+  description:
+    'Simple alignment of of object literal',
+  options:
+    {align: [':']},
+  input:
+    'var o = {\n' +
+    '  a: 1,\n' +
+    '  muchlonger: 2,\n' +
+    '}\n',
+  expected:
+    'var o = {\n' +
+    '  a          : 1,\n' +
+    '  muchlonger : 2,\n' +
+    '}\n',
+});
+
+helper.test({
+  description:
+    'Align tokens that already have whitespace before them',
+  options:
+    {align: [':']},
+  input:
+    'var o = {\n' +
+    '  a : 1,\n' +
+    '  muchlonger : 2,\n' +
+    '}\n',
+  expected:
+    'var o = {\n' +
+    '  a          : 1,\n' +
+    '  muchlonger : 2,\n' +
+    '}\n',
+});
