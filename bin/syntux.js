1c76da285c887b2b984b8bc9a5488a80addf9187
Felix Geisendörfer <felix@debuggable.com>
2011-10-31 23:14:07 +0100

    Initial approach for new cli tool

--- bin/syntux.js
+++ bin/syntux.js
@@ -1,44 +1,11 @@
 #!/usr/bin/env node
-var Syntux = require('..');
-var fs     = require('fs');
+var SyntuxCli = require('../lib/syntux_cli');
+var cli       = new SyntuxCli(process.argv, process.stdout, process.stderr);
 
-// @TODO Properly locate this, traverse from current dir
-var syntaxPath = process.cwd() + '/syntax.json';
+cli.execute(function(err) {
+  var code = (err)
+    ? 1
+    : 0;
 
-try{
-  var json = fs.readFileSync(syntaxPath, 'utf8');
-} catch (err) {
-  throw new Error('Could not find syntax file at: ' + syntaxPath);
-}
-
-try {
-  var options = JSON.parse(json);
-} catch (err) {
-  throw new Error('Could not parse syntax.json: ' + err);
-}
-
-var files = (function findJsFiles(dir) {
-  return fs
-    .readdirSync(dir)
-    .reduce(function(jsFiles, fileName) {
-      // @TODO Properly handle this
-      if (fileName === 'node_modules' || fileName === 'fixture' || fileName === 'tmp') return jsFiles;
-
-      var path = dir + '/' + fileName;
-      var stat = fs.statSync(path);
-
-      if (stat.isDirectory()) return jsFiles.concat(findJsFiles(path));
-
-      if (/\.js$/.test(fileName)) jsFiles.push(path);
-
-      return jsFiles;
-    }, []);
-})(process.cwd());
-
-files.forEach(function(path) {
-  console.log('Transforming: %s ...', path);
-
-  var source      = fs.readFileSync(path, 'utf8');
-  var transformed = Syntux.transform(source, options);
-  fs.writeFileSync(path, transformed, 'utf8');
+  process.exit(code);
 });
