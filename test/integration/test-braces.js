a41c08ad1bff66a22d608585d64e16f0fe24b861
Tobias von Klipstein <klipstein@uxebu.com>
2011-11-16 23:50:36 +0100

    Initial checkin of the braces plugin

---
+++ test/integration/test-braces.js
@@ -0,0 +1,58 @@
+var TransformHelper = require('../common').helper('transform_helper');
+var helper = TransformHelper.create();
+
+helper.test({
+  description:
+    'Simple brace test. No wrapping.',
+  options:
+    {braces: 'knr'},
+  input:
+    'function(){}',
+  expected:
+    'function(){}'
+});
+
+helper.test({
+  description:
+    'Simple brace test. Wrapping.',
+  options:
+    {braces: 'knr'},
+  input:
+    'function(){var foo="bar";}',
+  expected:
+    'function(){\nvar foo="bar";\n}\n'
+});
+
+helper.test({
+  description:
+    'Brace test with indentation.',
+  options:
+    {braces: 'knr'},
+  input:
+    '  \tfunction(){var foo="bar";}',
+  expected:
+    '  \tfunction(){\n  \tvar foo="bar";\n  \t}\n'
+});
+
+helper.test({
+  description:
+    'Brace test with spacing.',
+  options:
+    {braces: 'knr'},
+  input:
+    'function()\t\n {\n\t var foo="bar";}',
+  expected:
+    'function(){\nvar foo="bar";\n}\n'
+});
+
+//helper.test({
+//  description:
+//    'Modify braces according c style',
+//  options:
+//    {braces: "knr"},
+//  input:
+//    'var func = function()\n\n  {  /*what*/  switch(foo)  {  case "foo":if(){if(){}}}};',
+//  expected:
+//    'var func = function(){\n/*what*/  switch(foo){\ncase "foo":if(){\nif(){}}}};'
+//});
+
