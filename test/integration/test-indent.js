6b5b31660ec1ec38797f08680b6262873391d2e8
Stephan Seidt <evilhackerdude@gmail.com>
2011-11-22 22:13:51 +0100

    Implement tab-eating indent plugin

---
+++ test/integration/test-indent.js
@@ -0,0 +1,30 @@
+var TransformHelper = require('../common').helper('transform_helper');
+var helper = TransformHelper.create();
+
+helper.test({
+  description:
+    'Fix wrong indentation',
+  options:
+    {indent: '  '},
+  input:
+    'function foo() {\n' +
+    '\treturn;\n'        +
+    '}\n',
+  expected:
+    'function foo() {\n' +
+    '  return;\n'        +
+    '}\n'
+});
+
+helper.test({
+  description:
+    'Do not break custom indentation',
+  options:
+    {indent: '  '},
+  input:
+    '\tvar foo = 1,\n' +
+    '\t    bar = 2;',
+  expected:
+    '  var foo = 1,\n' +
+    '      bar = 2;'
+});
